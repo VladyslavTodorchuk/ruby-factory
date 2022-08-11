221b61b8c8a4690a2f11bf7e5df7acda2d6e5dde
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-08-11 15:59:18 +0300

    Add Factory classclear

--- lib/factory.rb
+++ lib/factory.rb
@@ -14,3 +14,47 @@
 # - to_a
 # - values_at
 # - ==, eql?
+
+# frozen_string_literal: true
+
+class Factory
+  def self.new(*class_arguments, &code_block)
+    Class.new do
+      attr_accessor(*class_arguments)
+
+      define_method :class_attributes do
+        class_arguments
+      end
+
+      def initialize(*argument_values)
+        raise ArgumentError, 'Wrong count of args' if argument_values.length != class_attributes.length
+
+        instance_hash = class_attributes.zip(argument_values)
+        instance_hash.each do |attribute, value|
+          instance_variable_set("@#{attribute}", value)
+        end
+      end
+
+      def [](argument)
+        if argument.instance_of? Integer
+          raise IndexError, 'No such attribute' unless (0...class_attributes.length).cover?(argument)
+
+          inst_name = instance_variables[argument]
+        else
+          inst_name = "@#{argument}"
+        end
+        instance_variable_get(inst_name)
+      end
+
+      def length
+        instance_variables.length
+      end
+
+      def size
+        instance_variables.length
+      end
+
+      class_eval(&code_block) if block_given?
+    end
+  end
+end
