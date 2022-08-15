32363907d387411e5e1f9ded905c7f6be68132a1
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-08-15 10:18:55 +0300

    Add create_class method

--- lib/factory.rb
+++ lib/factory.rb
@@ -18,89 +18,86 @@
 # - ==, eql? done
 
 class Factory
-  def self.new(*class_arguments, &code_block)
-    if class_arguments[0].instance_of? String
-      @name = class_arguments[0]
-      class_arguments.delete_at(0)
+  class << self
+    def new(*class_arguments, &code_block)
+      if class_arguments[0].instance_of? String
+        name = class_arguments[0]
+        class_arguments.delete_at(0)
+        self.const_set(name, create_class(*class_arguments,&code_block))
+      else
+        create_class(*class_arguments,&code_block)
+      end
     end
 
-    @new_class = Class.new do
-      attr_accessor(*class_arguments)
+    def create_class(*class_arguments, &code_block)
+      Class.new do
+        attr_accessor(*class_arguments)
 
-      define_method :initialize do |*argument_values|
-        raise ArgumentError, 'Wrong count of args' if argument_values.length != class_arguments.length
+        define_method :initialize do |*argument_values|
+          raise ArgumentError, 'Wrong count of args' if argument_values.size != class_arguments.size
 
-        instance_hash = class_arguments.zip(argument_values)
-        instance_hash.each do |attribute, value|
-          instance_variable_set("@#{attribute}", value)
+          instance_hash = class_arguments.zip(argument_values)
+          instance_hash.each do |attribute, value|
+            instance_variable_set("@#{attribute}", value)
+          end
         end
+
+        define_method :[] do |argument|
+          inst_name = argument.is_a?(Integer) ? instance_variables[argument] : "@#{argument}"
+          instance_variable_get(inst_name)
+        end
+
+        define_method :[]= do |argument, value|
+          inst_name = argument.is_a?(Integer) ? instance_variables[argument] : "@#{argument}"
+          instance_variable_set(inst_name, value)
+        end
+
+        define_method :values_at do |*values|
+          to_a.values_at(*values)
+        end
+
+        def length
+          instance_variables.length
+        end
+        alias :size :length
+
+        define_method :select do |&method|
+          attributes_values.select(&method)
+        end
+
+        define_method :members do
+          class_arguments
+        end
+
+        def ==(obj)
+          raise TypeError, 'Obj is nil' if obj.nil?
+
+          self.class == obj.class && self.to_a == obj.to_a
+        end
+        alias :eql? :==
+
+        define_method :to_a do
+          attributes_values
+        end
+
+        define_method :each_pair do |&block|
+          members.zip(attributes_values).to_h.each(&block)
+        end
+
+        define_method :each do |&block|
+          attributes_values.each(&block)
+        end
+
+        define_method :dig do |*args|
+          args.inject(self) { |value, elem| value[elem] if value }
+        end
+
+        define_method :attributes_values do
+          instance_variables.map { |symbol| instance_variable_get symbol }
+        end
+
+        class_eval(&code_block) if block_given?
       end
-
-      define_method :[] do |argument|
-        inst_name = if argument.instance_of? Integer
-                      instance_variables[argument]
-                    else
-                      "@#{argument}"
-                    end
-        instance_variable_get(inst_name)
-      end
-
-      define_method :[]= do |argument, value|
-        inst_name = if argument.instance_of? Integer
-                      instance_variables[argument]
-                    else
-                      "@#{argument}"
-                    end
-        instance_variable_set(inst_name, value)
-      end
-
-      define_method :values_at do |*values|
-        to_a.values_at(*values)
-      end
-
-      def length
-        instance_variables.length
-      end
-      alias :size :length
-
-      define_method :select do |&method|
-        attributes_values.select(&method)
-      end
-
-      define_method :members do
-        class_arguments
-      end
-
-      def ==(obj)
-        raise TypeError, 'Obj is nil' if obj.nil?
-
-        self.class == obj.class && self.to_a == obj.to_a
-      end
-      alias :eql? :==
-
-      define_method :to_a do
-        attributes_values
-      end
-
-      define_method :each_pair do |&block|
-        members.zip(attributes_values).to_h.each(&block)
-      end
-
-      define_method :each do |&block|
-        attributes_values.each(&block)
-      end
-
-      define_method :dig do |*args|
-        args.inject(self) { |value, elem| value[elem] if value }
-      end
-
-      define_method :attributes_values do
-        instance_variables.map { |symbol| instance_variable_get symbol }
-      end
-
-      class_eval(&code_block) if block_given?
     end
-
-    @name.nil? ? @new_class : self.const_set(@name, @new_class)
   end
 end
